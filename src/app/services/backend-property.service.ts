1aaa34378c7aef208b23992c5de340f58c84c30b
tanya1019 <tanya.vanneri19@gmail.com>
2023-09-06 21:06:35 +0530

    create property

---
+++ src/app/services/backend-property.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Property } from '../class/Property';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendPropertyService {
+
+  constructor(private httpClient : HttpClient) { }
+
+  postPropertyByCustid(newProperty : Property , custId:number) : Observable<any> {
+    return this.httpClient.post("http://localhost:8090/createProperty/" + custId, newProperty )
+  }
+
+}
