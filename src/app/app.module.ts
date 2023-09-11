346079b9d0c9e52de6e4ca4209984ad1738844a9
tanya1019 <tanya.vanneri19@gmail.com>
2023-09-11 12:32:32 +0530

    merged changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { CreateUserComponent } from './components/create-user/create-user.compon
 import { DisplayAllAdminPoliciesComponent } from './components/display-all-admin-policies/display-all-admin-policies.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { UserClaimComponent } from './components/user-claim/user-claim.component';
+import { PolicyPaymentsComponent } from './components/policy-payments/policy-payments.component';
 
 
 
@@ -40,8 +40,7 @@ import { UserClaimComponent } from './components/user-claim/user-claim.component
     CreateFeatureComponent,
         ProfileComponent,
         NavbarComponent,
-        UserClaimComponent
-        
+        PolicyPaymentsComponent,
   ],
   imports: [
     BrowserModule,
